b3b61e0c37429e2d546c767364dbd8dd4b7ae503
Alamin Usman Saleh <alaminusmansaleh43@gmail.com>
2025-01-12 10:50:53 +0100

    Na gyara Allowed hosts, Allah yasa yayi yanzu kan.

--- enir/settings.py
+++ enir/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-ALLOWED_HOSTS = config("ALLOWED_HOSTS")
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
